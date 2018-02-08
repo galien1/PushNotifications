a98d2dc303b964971153f93388785c92009033bc
galien1 <galienshadowstriker@gmail.com>
2018-02-07 20:14:07 -0500

    Update notification.js

--- notification.js
+++ notification.js
@@ -13,7 +13,7 @@ function notifyMe() {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
   else {
-    var notification = new Notification('Notification title', {
+    var notification = new Notification('Welcome!', {
       icon: 'https://i.stack.imgur.com/1rwJA.png?s=64&g=1',
       body: "Hey there! You've been notified!",
     });
